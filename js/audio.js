687f02144d4973d5194ef5522cfd332293ad2904
gegguli <47102440+gegguli@users.noreply.github.com>
2026-02-23 09:47:42 +0200

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,30 @@
+const audio = document.getElementById("audio");
 
+const JUMP_TIME = 30; // seconds
+
+function playAudio() {
+  audio.play();
+}
+
+function pauseAudio() {
+  audio.pause();
+}
+
+function togglePause() {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+}
+
+function seekToPoint() {
+  audio.currentTime = JUMP_TIME;
+}
+
+// Make functions globally accessible for ui.js
+window.AudioController = {
+  playAudio,
+  togglePause,
+  seekToPoint
+};
